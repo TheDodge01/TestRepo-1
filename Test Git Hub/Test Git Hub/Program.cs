0c1ce08d5f6dd073b8710b5a36a2b3da68a17482
Kris <softunikris@abv.bg>
2019-07-21 01:25:44 +0300

    Added some code: 1

---
+++ Test Git Hub/Test Git Hub/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test_Git_Hub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+
+            Console.WriteLine(a);
+        }
+    }
+}
